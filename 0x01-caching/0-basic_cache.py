a42503b90a302e692dcb6504678b604873567833
Your Name <you@example.com>
2024-08-29 17:45:16 +0000

    cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+Task 0: Basic dictionary
+'''
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''
+    A class `BasicCache` that inherits from `BaseCaching`
+    and is a caching system
+    '''
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
