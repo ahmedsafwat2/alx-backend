e4d765af6a4c7a842dc1a85448e9d671ec7cbe22
Your Name <you@example.com>
2024-08-29 20:39:13 +0000

    FIFO

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -23,7 +23,8 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS) and (self.cache_data.get(key, None) is None):
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and
+                self.cache_data.get(key, None) is None):
             iter_dict = iter(self.cache_data)
             first_element = next(iter_dict)
             self.cache_data.pop(first_element)
